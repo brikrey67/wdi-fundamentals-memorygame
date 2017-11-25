7855ba6ff28ae1549e6d6fab3cc6e808b5b58fd9
Brian Reynolds <brikrey@gmail.com>
2017-11-24 21:25:45 -0500

    Created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,43 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
 
-if(cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]){ 
-	// cards are equal
-	alert("You found a match!");
-	}
-	else { 
-	// cards are not equal
-	alert("Sorry, try again.");
+		// cards are equal
+		alert("You found a match!");
+		//console.log("You found a match!");	
+	} else { 
+		// cards are not equal
+		alert("Sorry, try again.");
+		//console.log("Sorry, try again.");
 	}
 }
+
+var flipCard = function(cardId) {
+/*	var cardOne = cards[cardId];
+	cardsInPlay.push(0);
+	console.log("User flipped " + cardOne);
+	var cardTwo = cards[1];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);*/
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if(cardsInPlay.length === 2) {
+		/*if (cardsInPlay[0] === cardsInPlay[1]){ 
+		// cards are equal
+		alert("You found a match!");
+		}
+		else { 
+		// cards are not equal
+		alert("Sorry, try again.");
+		}*/
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+
