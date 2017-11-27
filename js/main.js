25d5b0e7502829fcd333d1959ddd55504eb162aa
Brian Reynolds <brikrey@gmail.com>
2017-11-26 20:05:49 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -38,33 +38,43 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
-/*	var cardOne = cards[cardId];
-	cardsInPlay.push(0);
-	console.log("User flipped " + cardOne);
-	var cardTwo = cards[1];
-	cardsInPlay.push(cardTwo);
-	console.log("User flipped " + cardTwo);*/
+var flipCard = function() {
+
+	var cardId = this.getAttribute('data-id');
 
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("Card image: " + cards[cardId].cardImage);
 	console.log("Card suit: " + cards[cardId].suit);
 
+	// then a user clicks a card we want to display the image for the face
+	// use the setAttribute method to update the scr attribute of the image
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	if(cardsInPlay.length === 2) {
-		/*if (cardsInPlay[0] === cardsInPlay[1]){ 
-		// cards are equal
-		alert("You found a match!");
-		}
-		else { 
-		// cards are not equal
-		alert("Sorry, try again.");
-		}*/
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+
+	for (var i = 0; i < cards.length; i++) {
+		// create new img for DOM
+		var cardElement = document.createElement('img');
+		// set src attribute
+		cardElement.setAttribute('src', 'images/back.png');
+		// set data attribute that is not tied to style
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		//console.log("loop " + i);
+		//document.getElementsByTagName('div')[0].appendChild(cardElement);
+		document.getElementById('game-board').appendChild(cardElement);
+		}
+};
+
+/*flipCard(0);
+flipCard(2);*/
+
+createBoard();
 
 
